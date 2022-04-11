39e70650fcab951957e7f0310e7eb4f1a8b02efd
alvinarh <alvinarhidayati@gmail.com>
2022-04-11 14:10:27 +0700

    latihan minggu 6

---
+++ Minggu 4/Praktikum/CI3-upload/application/views/v_upload.php
@@ -0,0 +1,11 @@
+<head>
+	<title>malasngoding.com</title>
+</head>
+
+<body>
+	<center>
+		<h1>Membuat Upload File Dengan CodeIgniter 3 | MalasNgoding.com</h1>
+	</center> <?php echo $error; ?> <?php echo form_open_multipart('upload/aksi_upload'); ?> <input type="file" name="berkas" /> <br /><br /> <input type="submit" value="upload" /> </form>
+</body>
+
+</html>
